8be240ccc52c69b7fb20e1b51851bc82f8ba03f5
Eduardo <eduardokunmixyeah@gmail.com>
2024-06-17 01:06:24 -0400

    complete attendace

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AttendanceService } from './attendance.service';
+import { Attendance } from 'src/dtos/attendance.dto';
+import { DtoBaseResponse } from 'src/dtos/base-response.dto';
 
 @Controller('attendance')
-export class AttendanceController {}
+export class AttendanceController {
+
+    constructor(private attedanceService: AttendanceService){
+
+    }
+
+    @Post()
+    async saveAttencande(@Body() attendance: Attendance[]): Promise<DtoBaseResponse> {
+        return this.attedanceService.saveAttendance(attendance);
+    }
+    @Get()
+    async getAttencande(): Promise<boolean> {
+        return this.attedanceService.getAttendance();
+    }
+}
