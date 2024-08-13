5185e82e5d677de39cb39a706de0527085c43da3
Eduardo <eduardokunmixyeah@gmail.com>
2024-08-13 01:45:48 -0400

    upload files for topics, create table filesTopices

--- src/topics/topics.controller.ts
+++ src/topics/topics.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { TopicsService } from './topics.service';
-import { Activities, Topics } from '@prisma/client';
+import { FilesTopics, Topics } from '@prisma/client';
 import { DtoBaseResponse } from 'src/dtos/base-response.dto';
 import { DtoAddActivity, DtoAddTopics, DtoPutActivity, DtoPutTopics } from 'src/dtos/topics.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('topics')
 export class TopicsController {
@@ -22,6 +24,31 @@ export class TopicsController {
     async AddTopics(@Body() newTopics: DtoAddTopics): Promise<DtoBaseResponse>{
         return await this.topicsService.addTopics(newTopics);
     }
+    @Get('/file/:id')
+    async postConsultActivities(@Param('id') fileId: string): Promise<FilesTopics | null> {
+        return this.topicsService.consultFileTopic(fileId);
+    }
+    @Post('/file/:id')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './files_system',
+            filename: function (req, file, cb) {
+                cb(null, file.originalname);
+            },
+        }),
+    }))
+    async UploadFileTopics(
+        @UploadedFile() file: Express.Multer.File,
+        @Param('id') topicId: string
+    ) {
+
+    if (!file) {
+        throw new BadRequestException('Archivo no encontrado');
+    }
+
+    return await this.topicsService.uploadFileTopic(file, topicId);
+    }
+
     @Post('/activity')
     async AddActivity(@Body() newActivity: DtoAddActivity): Promise<DtoBaseResponse>{
         return await this.topicsService.addActivity(newActivity);
